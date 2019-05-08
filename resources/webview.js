e6779e8aca98b73ca4b1471e5bcdb026108d36c7
Grant Blakeman <hello@grantblakeman.com>
2019-05-08 12:02:59 -0700

    gui - stub in webview from template

---
+++ resources/webview.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+// disable the context menu (eg. the right click menu) to have a more native feel
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+});
+
+// call the plugin from the webview
+document.getElementById('button').addEventListener('click', () => {
+  window.postMessage('nativeLog', 'Called from the webview');
+});
+
+// call the wevbiew from the plugin
+window.setRandomNumber = (randomNumber) => {
+  document.getElementById('answer').innerHTML = `Random number from the plugin: ${randomNumber}`;
+};
+/* eslint-enable no-undef */
