e04dcafb6c5f9d3374a49c20e5141877d7570d97
Grant Blakeman <hello@grantblakeman.com>
2019-05-15 10:44:51 -0700

    tools - remove rgba converter (unused)

--- src/Tools.js
+++ src/Tools.js
@@ -1,14 +1,6 @@
+/* eslint-disable import/prefer-default-export */
 import { toArray } from 'util';
 
 // conversion function to give us full js Array functions from an NSArray object
 export const setArray = nsArray => toArray(nsArray);
-
-export const hexToRgb = (hex) => {
-  const r = parseInt(hex.slice(1, 3), 16);
-  const g = parseInt(hex.slice(3, 5), 16);
-  const b = parseInt(hex.slice(5, 7), 16);
-  const a = parseInt(hex.slice(7, 9), 16) / 255;
-
-  const rgbValue = `rgba(${r}, ${g}, ${b}, ${a * 100}%`;
-  return rgbValue;
-};
+/* eslint-enable import/prefer-default-export */
