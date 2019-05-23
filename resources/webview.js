8a09ee6cf636c330987ae8ce138fbf5a2964438b
Grant Blakeman <hello@grantblakeman.com>
2019-05-23 11:21:28 -0700

    webview - re-org js

--- resources/webview.js
+++ resources/webview.js
@@ -1,44 +1,90 @@
 /* eslint-disable no-undef */
-
 /**
- * @description Disables the system-level contextual menu (eg. the right click menu)
- * to give the webview a more native feel.
- *
- * @listens document
- */
-document.addEventListener('contextmenu', (e) => {
-  e.preventDefault();
-});
-
-/**
-* @description Array containing the UI elements to watch for input.
-*
-* @kind constant
-* @name triggerElements
-* @type {array}
-*/
-const triggerElements = document.querySelectorAll('.action-trigger');
-
-/**
- * @description Takes a string message and logs it at one of 2 levels (normal or error).
+ * @description A listener for `DOMContentLoaded`.
  *
  * @kind function
- * @param {array} triggerElements Array containing the UI elements to watch for input.
+ * @name ready
+ * @listens document
+ * @param {Function} fn The function to call once the DOM is loaded.
  */
-Array.from(triggerElements).forEach((trigger) => {
-  document.getElementById(trigger.id).addEventListener('click', () => {
-    switch (trigger.id) {
-      case 'close':
-        window.postMessage('closeWindow');
-        break;
-      case 'label':
-        window.postMessage('labelLayer');
-        break;
-      default:
-        window.postMessage('nativeLog', `Called #${trigger.id} from the webview`);
-    }
-    return null;
-  });
-});
+const ready = (fn) => { // eslint-disable-line no-unused-vars
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    fn();
+  }
+};
 
+/**
+* @description A wrapper to contain all of the webview functionality.
+*
+* @kind function
+* @name AutoSpecWebview
+* @type {module}
+*/
+const AutoSpecWebview = {
+  /**
+   * @description The initializer that is called once the DOM is loaded.
+   *
+   * @kind function
+   * @name init
+   */
+  init: () => {
+    AutoSpecWebview.disableRightClick();
+    AutoSpecWebview.watchTriggers();
+  },
+  /**
+   * @description Disables the system-level contextual menu (eg. the right click menu)
+   * to give the webview a more native feel.
+   *
+   * @kind function
+   * @name disableRightClick
+   * @listens document
+   */
+  disableRightClick: () => {
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+    });
+  },
+  /**
+   * @description Sets listeners on all GUI trigger elements, watches for clicks,
+   * and dispatches to the appropriate `GUI.js` actions.
+   *
+   * @kind function
+   * @name watchTriggers
+   */
+  watchTriggers: () => {
+    /**
+    * @description Array containing the UI elements to watch for input.
+    *
+    * @kind constant
+    * @name triggerElements
+    * @type {array}
+    */
+    const triggerElements = document.querySelectorAll('.action-trigger');
+
+    /**
+     * @description Takes a string message and logs it at one of 2 levels (normal or error).
+     *
+     * @kind function
+     * @param {array} triggerElements Array containing the UI elements to watch for input.
+     */
+    Array.from(triggerElements).forEach((trigger) => {
+      document.getElementById(trigger.id).addEventListener('click', () => {
+        switch (trigger.id) {
+          case 'close':
+            window.postMessage('closeWindow');
+            break;
+          case 'label':
+            window.postMessage('labelLayer');
+            break;
+          default:
+            window.postMessage('nativeLog', `Called #${trigger.id} from the webview`);
+        }
+        return null;
+      });
+    });
+  },
+};
+ready(AutoSpecWebview.init);
 /* eslint-enable no-undef */
