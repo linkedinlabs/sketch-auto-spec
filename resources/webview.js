ab28070b69994b3c65e985ae639d552a6d5c4b8c
Grant Blakeman <hello@grantblakeman.com>
2019-06-28 19:22:57 -0700

    gui - re-enable measure button

--- resources/webview.js
+++ resources/webview.js
@@ -77,6 +77,9 @@ const AutoSpecWebview = {
           case 'annotate':
             window.postMessage('annotateLayer');
             break;
+          case 'measure':
+            window.postMessage('annotateMeasurement');
+            break;
           case 'bind':
             window.postMessage('drawBoundingBox');
             break;
